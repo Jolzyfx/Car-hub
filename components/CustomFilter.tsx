2bb33d9a57abbbe5605c37b3fd9a0f2b5c316487
JolzyFx Codes <jolzyanth@gmail.com>
2023-06-17 11:10:10 +0100

    getting the Apis

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,10 @@
+import { CustomButtonProps } from '@/types'
+import React from 'react'
+
+const CustomFilter = ({title, containerStyles,btnType, handleClick}: CustomButtonProps) => {
+  return (
+    <div>CustomFilter</div>
+  )
+}
+
+export default CustomFilter
